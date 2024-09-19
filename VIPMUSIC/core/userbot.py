a7629b4e6b4f4c908703f02e99775ff92eeda7ff
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-09-19 20:02:19 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("ll_NOBITA_BOT_DEVLOPER_ll")
+                await self.one.join_chat("NOBITA_BOT_MAKER_STATUS")
+                await self.one.join_chat("NOBITA_SUPPORT")
+                await self.one.join_chat("NOBITA_ALL_BOT")
             except:
                 pass
             assistants.append(1)
@@ -93,10 +93,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TheVIP")
-                await self.two.join_chat("VIPSupport")
-                await self.two.join_chat("THE-VIP-BOY-OP")
-                await self.two.join_chat("TheTeamVk")
+                await self.two.join_chat("ll_NOBITA_BOT_DEVLOPER_ll")
+                await self.two.join_chat("NOBITA_SUPPORT")
+                await self.two.join_chat("NOBITA_ALL_BOT")
+                await self.two.join_chat("NOBITA_BOT_MAKER_STATUS")
             except:
                 pass
             assistants.append(2)
@@ -121,10 +121,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TheVIP")
-                await self.three.join_chat("VIPSupport")
-                await self.three.join_chat("THE-VIP-BOY-OP")
-                await self.three.join_chat("TheTeamVk")
+                await self.three.join_chat("ll_NOBITA_BOT_DEVLOPER_ll")
+                await self.three.join_chat("NOBITA_BOT_MAKER_STATUS")
+                await self.three.join_chat("NOBITA_SUPPORT")
+                await self.three.join_chat("NOBITA_ALL_BOT")
             except:
                 pass
             assistants.append(3)
@@ -149,10 +149,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TheVIP")
-                await self.four.join_chat("VIPSupport")
-                await self.four.join_chat("THE-VIP-BOY-OP")
-                await self.four.join_chat("TheTeamVk")
+                await self.four.join_chat("NOBITA_ALL_BOT")
+                await self.four.join_chat("ll_NOBITA_BOT_DEVLOPER_ll")
+                await self.four.join_chat("NOBITA_SUPPORT")
+                await self.four.join_chat("NOBITA_BOT_MAKER_STATUS")
             except:
                 pass
             assistants.append(4)
@@ -177,10 +177,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TheVIP")
-                await self.five.join_chat("VIPSupport")
-                await self.five.join_chat("THE-VIP-BOY-OP")
-                await self.five.join_chat("TheTeamVk")
+                await self.five.join_chat("NOBITA_BOT_MAKER_STATUS")
+                await self.five.join_chat("NOBITA_SUPPORT")
+                await self.five.join_chat("NOBITA_ALL_BOT")
+                await self.five.join_chat("ll_NOBITA_BOT_DEVLOPER_ll")
             except:
                 pass
             assistants.append(5)
