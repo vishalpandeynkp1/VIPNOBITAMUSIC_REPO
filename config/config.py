9e56088c8ac6795a2a9826ddeba83f3b0ec62fec
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-09-19 18:52:26 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -52,7 +52,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/THE-VIP-BOY-OP/Extra-Plugin",
+    "https://github.com/vishalpandeynkp1/NOBITA-EXTRA-PLUGIN/Extra-Plugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
