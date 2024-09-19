68eab9cdaa7ab084980e5795c83d051291107a62
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-09-19 19:55:38 +0530

    Update mongo.py

--- VIPMUSIC/core/mongo.py
+++ VIPMUSIC/core/mongo.py
@@ -1,9 +1,8 @@
+# Copyright (C) 2024 by VISHAL-PANDEY@Github, < https://github.com/vishalpandeynkp1 >.
 #
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
-#
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# This file is part of < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO/blob/master/LICENSE >
 #
 # All rights reserved.
 #
