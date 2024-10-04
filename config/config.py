96b61d9941496b34ec4fa1fd4a18a26ed98b6b80
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-10-05 00:01:08 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -111,7 +111,7 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/Nobita_All_Bot"
 )  # Example:- https://t.me/VIP_CREATORS
 SUPPORT_GROUP = getenv(
-    "BOYS_STATUS_GROUP", "https://t.me/NOBITA_BOT_MAKER_STATUS"
+    "BOYS_STATUS_GROUP", "https://t.me/NOBITA_SUPPORT"
 )  # Example:- https://t.me/TG_FRIENDSS
 
 SUPPORT_CHAT = getenv(
@@ -211,67 +211,67 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/WjQ.jpg",
 )
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 PLAYLIST_IMG_URL = getenv(
     "PLAYLIST_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 GLOBAL_IMG_URL = getenv(
     "GLOBAL_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 STATS_IMG_URL = getenv(
     "STATS_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 TELEGRAM_AUDIO_URL = getenv(
     "TELEGRAM_AUDIO_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 TELEGRAM_VIDEO_URL = getenv(
     "TELEGRAM_VIDEO_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 STREAM_IMG_URL = getenv(
     "STREAM_IMG_URL",
-    "https://graph.org/file/99c2b5a447a51cea148b1.mp4",
+    "https://envs.sh/0-l.jpg",
 )
 
 SOUNCLOUD_IMG_URL = getenv(
     "SOUNCLOUD_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 YOUTUBE_IMG_URL = getenv(
     "YOUTUBE_IMG_URL",
-    "https://graph.org/file/638f56d8b53c137c20480.jpg",
+    "https://envs.sh/0-l.jpg",
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
     "SPOTIFY_ARTIST_IMG_URL",
-    "https://graph.org/file/99c2b5a447a51cea148b1.mp4",
+    "https://envs.sh/0-l.jpg",
 )
 
 SPOTIFY_ALBUM_IMG_URL = getenv(
     "SPOTIFY_ALBUM_IMG_URL",
-    "https://graph.org/file/99c2b5a447a51cea148b1.mp4",
+    "https://envs.sh/0-l.jpg",
 )
 
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
     "SPOTIFY_PLAYLIST_IMG_URL",
-    "https://graph.org/file/99c2b5a447a51cea148b1.mp4",
+    "https://envs.sh/0-l.jpg",
 )
 
 
@@ -312,7 +312,7 @@ if GITHUB_REPO:
 
 
 if PING_IMG_URL:
-    if PING_IMG_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if PING_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
             print(
                 "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -320,7 +320,7 @@ if PING_IMG_URL:
 
 
 if PLAYLIST_IMG_URL:
-    if PLAYLIST_IMG_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if PLAYLIST_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
             print(
                 "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -328,7 +328,7 @@ if PLAYLIST_IMG_URL:
 
 
 if GLOBAL_IMG_URL:
-    if GLOBAL_IMG_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if GLOBAL_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
             print(
                 "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -336,7 +336,7 @@ if GLOBAL_IMG_URL:
 
 
 if STATS_IMG_URL:
-    if STATS_IMG_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if STATS_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", STATS_IMG_URL):
             print(
                 "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -344,7 +344,7 @@ if STATS_IMG_URL:
 
 
 if TELEGRAM_AUDIO_URL:
-    if TELEGRAM_AUDIO_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if TELEGRAM_AUDIO_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
             print(
                 "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
@@ -352,7 +352,7 @@ if TELEGRAM_AUDIO_URL:
 
 
 if STREAM_IMG_URL:
-    if STREAM_IMG_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if STREAM_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", STREAM_IMG_URL):
             print(
                 "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -360,7 +360,7 @@ if STREAM_IMG_URL:
 
 
 if SOUNCLOUD_IMG_URL:
-    if SOUNCLOUD_IMG_URL != "https://graph.org/file/99c2b5a447a51cea148b1.mp4":
+    if SOUNCLOUD_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
             print(
                 "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -368,7 +368,7 @@ if SOUNCLOUD_IMG_URL:
 
 
 if YOUTUBE_IMG_URL:
-    if YOUTUBE_IMG_URL != "https://graph.org/file/99c2b5a447a51cea148b1.mp4":
+    if YOUTUBE_IMG_URL != "https://envs.sh/0-U.jpg":
         if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
             print(
                 "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
@@ -376,7 +376,7 @@ if YOUTUBE_IMG_URL:
 
 
 if TELEGRAM_VIDEO_URL:
-    if TELEGRAM_VIDEO_URL != "https://graph.org/file/638f56d8b53c137c20480.jpg":
+    if TELEGRAM_VIDEO_URL != "https://graph.org/file/99c2b5a447a51cea148b1.mp4":
         if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
             print(
                 "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
