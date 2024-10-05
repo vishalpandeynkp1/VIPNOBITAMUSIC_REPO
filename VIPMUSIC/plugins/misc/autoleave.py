0e633064ede7098d41bb7c6581d687b6cb74be22
vishalpandeynkp1 <nishathakur.nkp@gmail.com>
2024-10-05 09:28:58 +0530

    Update autoleave.py

--- VIPMUSIC/plugins/misc/autoleave.py
+++ VIPMUSIC/plugins/misc/autoleave.py
@@ -38,8 +38,8 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if chat_id not in [
                                 config.LOG_GROUP_ID,
-                                -1002159045835,
-                                -1002146211959,
+                                -1002223516578,
+                                -1002165424124,
                             ]:
                                 if left == 20:
                                     continue
