f106938145ec7345804859cf312ce381551b7f28
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-10-25 22:36:36 +0530

    Update __init__.py

--- VIPMUSIC/__init__.py
+++ VIPMUSIC/__init__.py
@@ -1,5 +1,12 @@
-import json
-import os
+
+#
+# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+#
+# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
 
 from VIPMUSIC.core.bot import VIPBot
 from VIPMUSIC.core.dir import dirr
@@ -9,18 +16,26 @@ from VIPMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
+# Bot Client
+
+# Directories
 dirr()
 
+# Check Git Updates
 git()
 
+# Initialize Memory DB
 dbb()
 
+# Heroku APP
 heroku()
 
+# Load Sudo Users from DB
 sudo()
 
 app = VIPBot()
 
+# Assistant Client
 userbot = Userbot()
 
 from .platforms import *
@@ -33,12 +48,3 @@ Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
 HELPABLE = {}
-
-YOUTUBE = {
-    "access_token": "ya29.a0AcM612x6xjTKmTpSHstn1vpFe4R-4nlC-a537bZMFvBAKrDA7MdjgKj71_dLwijsrP5ZLSKFIUNQyHpFOjGh5ifATiFHyXhc4sArPYlbIa_9PqfVJMyfDWLa8vw19SZwHo--fxjDusyV5nqvoGYNcIqyTN8zG7gJl3YIvcSa0P0-rLQNAs0AaCgYKAb0SARMSFQHGX2MivXte9hRxxKS07uayRvAw7w0187",
-    "expires": 1729406829.524444,
-    "refresh_token": "1//05vYI0c8OP0b4CgYIARAAGAUSNwF-L9IrJvP8EzLj-4wkJD-hYD9y1fXRNSGS9CjEQ1YwRxFw1OjatSgXsGooDbs5QcqAPOs3TvM",
-    "token_type": "Bearer",
-}
-
-os.environ["TOKEN_DATA"] = json.dumps(YOUTUBE)
