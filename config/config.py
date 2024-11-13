48407727c1f8e157b796ee6a949e87e533d7d4a3
HYPER7147 <op0395687@gmail.com>
2024-11-13 21:22:39 +0530

    HYPER

--- config/config.py
+++ config/config.py
@@ -221,67 +221,67 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://graph.org/file/26102d4a6bb7f72681e58-1ef3b6f387a4f41118.jpg",
+    "https://graph.org/file/006dd75496c03ebb7c904-3541ad6fc6826f0670.jpg",
 )
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://graph.org/file/263c5c77829cbc7bfa3bd-0e86cd12c33d22d454.jpg",
+    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
 )
 
 PLAYLIST_IMG_URL = getenv(
     "PLAYLIST_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 GLOBAL_IMG_URL = getenv(
     "GLOBAL_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 STATS_IMG_URL = getenv(
     "STATS_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 TELEGRAM_AUDIO_URL = getenv(
     "TELEGRAM_AUDIO_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 TELEGRAM_VIDEO_URL = getenv(
     "TELEGRAM_VIDEO_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 STREAM_IMG_URL = getenv(
     "STREAM_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 SOUNCLOUD_IMG_URL = getenv(
     "SOUNCLOUD_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 YOUTUBE_IMG_URL = getenv(
     "YOUTUBE_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
     "SPOTIFY_ARTIST_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 SPOTIFY_ALBUM_IMG_URL = getenv(
     "SPOTIFY_ALBUM_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
     "SPOTIFY_PLAYLIST_IMG_URL",
-    "https://graph.org/file/2f8b9c602d565c0127f10-2c9ac001dd4159f84f.jpg",
+    "https://graph.org/file/ca453623aeca3ec92a254-e8e25eadac3bd08672.jpg",
 )
 
 
