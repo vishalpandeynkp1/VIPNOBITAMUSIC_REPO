5a5c035ce7ca661e95739f323639917345784304
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-11-21 10:18:57 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -54,7 +54,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/vishalpandeynkp1/NOBITA-EXTRA-PLUGIN",
+    "",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
